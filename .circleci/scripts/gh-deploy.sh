f1736631c2942354cdab16b15eff96a5bff016fb
Rafał Masiarek <rafal@masiarek.pl>
2021-08-29 17:36:46 +0200

    add .circleci

---
+++ .circleci/scripts/gh-deploy.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+set -o xtrace
+
+# Set the name of the folder that will be created in the parent
+# folder of your repo folder, and which will temporarily
+# hold the generated content.
+temp_folder=$(mktemp -d -t _gh-pages-temp-XXXXXXXXXX)
+
+# Make sure our main code runs only if we push the master branch
+if [ "$CIRCLE_BRANCH" == "main" ]; then
+
+    git config credential.helper 'cache --timeout=120'
+    git config user.email "${GITHUB_EMAIL}"
+    git config user.name "${GITHUB_USERNAME}"
+
+	# Store the last commit message from master branch
+	last_message=$(git show -s --format=%s master)
+
+	# Move the generated site in our temp folder
+	mv _site ../${temp_folder}
+
+	# Checkout the gh-pages branch and clean it's contents
+	git checkout gh-pages
+	rm -rf *
+
+	# Copy the site content from the temp folder and remove the temp folder
+	cp -r ../${temp_folder}/* .
+	rm -rf ../${temp_folder}
+
+	# Commit and push our generated site to GitHub
+	git add -A
+	git commit --allow-empty -m "Page release ${CIRCLE_BUILD_NUM} from ${CIRCLE_BRANCH}" -m "$last_message"
+	git push -q --force https://${GITHUB_TOKEN}@github.com/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}.git
+	git tag -a "release_${CIRCLE_BRANCH}_${CIRCLE_BUILD_NUM}" -m "Release based on build ${CIRCLE_BUILD_NUM}, status ${CIRCLE_BUILD_URL}"
+	git push -q --tags https://${GITHUB_TOKEN}@github.com/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}.git
+
+	# Go back to the main branch
+	git checkout main
+else
+	echo "Not main branch. Skipping build"
+fi
